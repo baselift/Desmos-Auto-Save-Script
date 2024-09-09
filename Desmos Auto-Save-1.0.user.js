f46fa77d5275180a6363b5566cdddb0761bc6323
Eric Song <59805043+baselift@users.noreply.github.com>
2024-09-08 22:05:57 -0400

    Added script file

---
+++ Desmos Auto-Save-1.0.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Desmos Auto-Save
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Auto-save desmos because it is not a feature for some reason
+// @author       Eric Song
+// @match        https://www.desmos.com/calculator/*
+// @icon         https://cdn.discordapp.com/attachments/1099736002651754590/1099736058972872774/communityIcon_23j1hfiv5vc21.png
+// @grant        none
+// ==/UserScript==
+
+var checkForSaveBttnThenClick = function() {
+    'use strict';
+    var results = xpath("//span[contains(@class, 'dcg-action-save') and not(contains(@class, 'dcg-disabled'))]");
+    if (results.length == 1 && results[0].innerText == "Save") {
+        var saveKeys = new KeyboardEvent("keydown", {key: 's', code: 'KeyS', ctrlKey: true});
+        document.dispatchEvent(saveKeys);
+    }
+};
+
+var xpath = function (xpathToExecute) {
+    var result = [];
+    var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (var i = 0; i < nodesSnapshot.snapshotLength; i++) {
+        result.push(nodesSnapshot.snapshotItem(i));
+    }
+    return result;
+};
+
+(function() {
+    'use strict';
+    setInterval(checkForSaveBttnThenClick, 5000);
+})();
